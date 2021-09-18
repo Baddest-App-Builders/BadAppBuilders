b4bde8a0dd3251ba65a6b8374057d11f5660a7b0
Michael Nazareth <mcnazareth@gmail.com>
2021-09-18 17:57:31 -0400

    almost there guys

--- my-app/src/components/youtube.js
+++ my-app/src/components/youtube.js
@@ -23,3 +23,14 @@ const firebaseConfig = {
 //     key: KEY,
 //   },
 // });
+
+// rules_version = '2';
+// service cloud.firestore {
+//   match /databases/{database}/documents {
+//     match /{document=**} {
+//       allow read, write: if
+//           request.time < timestamp.date(2021, 10, 18);
+//     }
+//   }
+// }
+//database rules for 30 day challenge.
